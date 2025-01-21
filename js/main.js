6f9bd2bf9c317c1475c171f9c5d46ca61b563765
y0uk1 <y0uk10126@gmail.com>
2025-01-20 17:38:25 -0700

    #9 add brand wagyu map

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { BrandMap } from "./brandMap.js";
 import { RankingBoard } from "./rankingBoard.js";
 import { WagyuExportChart } from "./wagyuExportChart.js";
 
@@ -10,6 +11,7 @@ const draw = async () => {
 
   let rankingKey;
 
+  const brandMap = new BrandMap("#brand-map");
   const rankingBoard = new RankingBoard("#ranking-board", rankingData);
   const wagyuExportChart = new WagyuExportChart(
     "#wagyu-export-chart",
